ce72a26a6a9875fc59db9e5bca849ab4b7d92987
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-04-01 13:39:16 +1000

    Complete hex_colours.py exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+"""
+CP1404 - Practical 5 - Jacob Finger
+Hexadecimal colour code lookup
+"""
+
+HEX_CODES = {"VioletRed": "	#d02090", "turquoise1": "#00f5ff", "ForestGreen": "#228b22", "DimGray": "#696969",
+             "goldenrod2": "#eeb422", "LightCoral": "#f08080", "magenta": "#ff00ff", "red1": "#ff0000",
+             "SeaGreen1": "#54ff9f", "sienna1": "#ff8247"}
+print(HEX_CODES)
+
+hex_name = input("Enter the colour name: ")
+while hex_name != "":
+    if hex_name in HEX_CODES:
+        print("The hex code of colour {} is {}".format(hex_name, HEX_CODES[hex_name]))
+    else:
+        print("Invalid colour name, please try again.")
+    hex_name = input("Enter the colour name: ")
