81ffd2ae994caa36a3ec9db8cc62df7561ec7cfb
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-04-19 12:17:07 +1000

    Remove test print statement and fix spacing for HEX_CODES

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,10 +3,9 @@ CP1404 - Practical 5 - Jacob Finger
 Hexadecimal colour code lookup
 """
 
-HEX_CODES = {"VioletRed": "	#d02090", "turquoise1": "#00f5ff", "ForestGreen": "#228b22", "DimGray": "#696969",
+HEX_CODES = {"VioletRed": "#d02090", "turquoise1": "#00f5ff", "ForestGreen": "#228b22", "DimGray": "#696969",
              "goldenrod2": "#eeb422", "LightCoral": "#f08080", "magenta": "#ff00ff", "red1": "#ff0000",
              "SeaGreen1": "#54ff9f", "sienna1": "#ff8247"}
-print(HEX_CODES)
 
 hex_name = input("Enter the colour name: ")
 while hex_name != "":
