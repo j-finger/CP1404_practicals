025e94a7f50630820ef875ef99e351f038e74a52
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-04-19 12:18:10 +1000

    Remove test print statement

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,7 +5,6 @@ State names in a dictionary
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
 state_code = (input("Enter short state: ")).upper()
 while state_code != "":
