4dcc58addc9b0abd1e9b857c125f32f4bfafe447
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-04-26 09:44:27 +1000

    Add convert_miles_km.py

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,6 +1,5 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
 from kivy.app import StringProperty
 
 
@@ -8,19 +7,28 @@ class ConvertMilesToKilometer(App):
     message = StringProperty()
 
     def build(self):
-        Window.size = (300, 200)
+        """Build Program GUI from file convert_miles_km.kv"""
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_increment(self, increment_number):
-        if self.message.isdigit():
-            self.message = str(int(self.message) + increment_number)
-        elif self.message == '' or not self.message.isdigit():
-            self.message = '0'
+    def handle_increment(self, user_input, increment):
+        number_to_increment = self.string_to_float(user_input)
+        number_to_increment += increment
+        self.root.ids.input_number.text = str(number_to_increment)
+        self.handle_convert(user_input)
 
-    def handle_convert(self):
-        self.message = self.root.ids.input_number.text
+    def handle_convert(self, user_input):
+        user_input = self.string_to_float(user_input)
+        self.message = str(user_input * 1.609)
+
+    @staticmethod
+    def string_to_float(text):
+        try:
+            number = float(text)
+        except ValueError:
+            number = 0.0
+        return number
 
 
 ConvertMilesToKilometer().run()
