3d3eab79346597c36bd4eb6fa49139c5ca0e2bf0
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-04-26 10:05:59 +1000

    Finish stage 2 of convert_miles_km

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,6 +1,9 @@
+"""CP1404 Practical 07 - Jacob Finger - Convert Miles to Km GUI Program"""
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.app import StringProperty
+from kivy.core.window import Window
+KILOMETERS_PER_MILE = 1.609
 
 
 class ConvertMilesToKilometer(App):
@@ -8,22 +11,26 @@ class ConvertMilesToKilometer(App):
 
     def build(self):
         """Build Program GUI from file convert_miles_km.kv"""
+        Window.size = (300, 175)
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_increment(self, user_input, increment):
+        """Increments given input with given increment"""
         number_to_increment = self.string_to_float(user_input)
         number_to_increment += increment
         self.root.ids.input_number.text = str(number_to_increment)
         self.handle_convert(user_input)
 
     def handle_convert(self, user_input):
+        """Converts given input from kilometers to miles"""
         user_input = self.string_to_float(user_input)
-        self.message = str(user_input * 1.609)
+        self.message = str(user_input * KILOMETERS_PER_MILE)
 
     @staticmethod
     def string_to_float(text):
+        """Converts text to a float or resets input to 0.0 for a non-numerical input"""
         try:
             number = float(text)
         except ValueError:
