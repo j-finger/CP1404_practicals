657586e0354b2a438a44594b848cadae01939482
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-05-28 15:37:35 +1000

    Refactor variable name in get_fixed_filename
    Remove unnecessary code from main()

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -6,56 +6,29 @@ import os
 
 
 def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
+    """Iterate through all directories and rename files correctly"""""
 
-    # Change to desired directory
-    os.chdir('Lyrics/Christmas')
+    os.chdir('Lyrics')
 
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
-
-
-def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    split_word = ""
-    split_word.isupper()
-    for i, c in enumerate(new_name):
-        if c.isupper() and new_name[i-1].isupper():
-            split_word += '_'
-        if c.isupper() and new_name[i-1].islower() and i != 0:
-            split_word += '_'
-        split_word += c
-    return split_word.title()
-
-
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics/Christmas')
     for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
         for filename in filenames:
             file_and_path = os.path.join(directory_name, filename)
             new_name = os.path.join(directory_name, get_fixed_filename(filename))
             os.rename(file_and_path, new_name)
 
 
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    split_names = ""
+    split_names.isupper()
+    for i, c in enumerate(new_name):
+        if c.isupper() and new_name[i-1].isupper():  # Split 2 uppercase characters
+            split_names += '_'
+        if c.isupper() and new_name[i-1].islower() and i != 0:  # Split an uppercase and lowercase character
+            split_names += '_'
+        split_names += c
+    return split_names.title()
+
+
 main()
-# demo_walk()
