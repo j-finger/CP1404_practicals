ebf247bd29efd685a080110b48736dda06636af5
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-05-28 14:16:56 +1000

    Finish os_demos.py exercise

--- prac_09/os_demos.py
+++ prac_09/os_demos.py
@@ -17,9 +17,10 @@ def main():
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
     # Make a new directory
-    # The next time you run this, it will crash if the directory exists
-    # TODO: Use exception handling to avoid the crash (just pass)
-    os.mkdir('temp')
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
 
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
@@ -30,9 +31,8 @@ def main():
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
 
-        # TODO: Try these options one at a time
         # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
+        os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
         # shutil.move(filename, 'temp/' + new_name)
@@ -53,8 +53,11 @@ def demo_walk():
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
 
-        # TODO: add a loop to rename the files
+        for filename in filenames:
+            file_and_path = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(file_and_path, new_name)
 
 
-main()
-# demo_walk()
+# main()
+demo_walk()
