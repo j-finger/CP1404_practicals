a6d5df54a1d9076eb2ea33623eeff3a1d6d1ccec
Jacob Finger <jacob.finger@my.jcu.edu.au>
2021-06-06 15:06:42 +1000

    Add default name to Car class
    Add functional testing of Car class & repeat_string function

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -2,14 +2,13 @@
 CP1404/CP5632 Practical
 Testing demo using assert and doctest
 """
-
 import doctest
 from prac_06.car import Car
 
 
-def repeat_string(s, n):
+def repeat_string(s='string', n=2):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return " ".join(s for i in range(n))
 
 
 def is_long_word(word, length=5):
@@ -22,7 +21,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -32,20 +31,12 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
-    # assert test with custom message,
-    # used to see if Car's init method sets the odometer correctly
-    # this should pass (no output)
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
+    assert test_car.fuel == 0
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10
 
 
 run_tests()
